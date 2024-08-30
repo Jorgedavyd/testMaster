2fe616ee8300bff9332aeec55bb9fefe8c235069
Jorgedavyd <jorged.encyso@gmail.com>
2024-08-30 14:54:07 -0400

    setting up func for druid -> grafana

--- API.py
+++ API.py
@@ -5,25 +5,44 @@ import json
 import pandas as pd
 import aioftp
 
+## to druid
 class API:
-    def __init__(self) -> None:
-        self.ftp: str = ftp
+    endpoint: str = 'http://localhost:8081/druid'
+    ftp: str = 'dfinir'
 
-    async def download_url(self, url: str):
-        async with
+    def __init__(self) -> None:
+
+    async def download_url(self, session, url: str) -> Any:
+        async with session.get(url, ssl = False) as request:
+            data = await request.json()
+            return data
 
     async def getPaths(self, url: str) -> None:
 
     def getPathTasks(self) -> List[Coroutine]:
         return [getPaths()
 
+    def processFile(self) ->
+    def __call__(self) -> None:
+        with aiohttp.ClientSession() as session:
+            await asyncio.gather(*self.getDownloadJSONTasks())
+            await asyncio.gather(*slef.getDruidTasks())
+
+    def getDruidTasks(self) -> List[Coroutine]:
+        return [self.singleToDruid() for _ in ]
+
+    def getDownloadJSONTasks(self) -> List[Coroutine]:
+        return [self.download_url() for _ in ]
+
+    def singleToDruid(self, session, data) -> None:
+        async with session.post(self.endpoint, json=data) as response:
+            if response.status == 200:
+                response_data = await response.data()
+            else:
+                print('Error')
+                response_test = await response.text()
+                print('Error: ')
+
     def get_ftp_tasks(self) -> None:
         return [download_url]
-
-class Formatter:
-    def __init__(self) -> None:
-        pass
-
-class Dataset:
-    self.result = 'result'
 
